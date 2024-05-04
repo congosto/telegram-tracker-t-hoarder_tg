d0814f57418f151fdec9d984779fb855515e2ff2
Mariluz Congosto <mariluz.congosto@gmail.com>
2024-05-04 17:54:15 +0200

    Cambios de la nueva versión de telegramtracker-t-hoarder_tg

--- main.py
+++ main.py
@@ -4,17 +4,21 @@
 import pandas as pd
 import argparse
 import asyncio
+import nest_asyncio # change by congosto
 import json
 import time
 import sys
 import os
-
+# import submodules
 # import Telegram API submodules
 from api import *
 from utils import (
-	get_config_attrs, JSONEncoder, create_dirs, cmd_request_type,
-	write_collected_chats
+	get_config_attrs, JSONEncoder, create_dirs, 
+	write_collected_chats,get_last_download_context,
+	put_last_download_context,store_channels_download,store_channels_related
 )
+
+
 
 '''
 
@@ -26,15 +30,10 @@ parser = argparse.ArgumentParser(description='Arguments.')
 parser.add_argument(
 	'--telegram-channel',
 	type=str,
-	required='--batch-file' not in sys.argv,
+	required=True,
 	help='Specifies a Telegram Channel.'
 )
-parser.add_argument(
-	'--batch-file',
-	type=str,
-	required='--telegram-channel' not in sys.argv,
-	help='File containing Telegram Channels, one channel per line.'
-)
+
 parser.add_argument(
 	'--limit-download-to-channel-metadata',
 	action='store_true',
@@ -55,27 +54,11 @@ parser.add_argument(
 
 
 
-
-'''
-
-Updating data
-'''
-parser.add_argument(
-	'--min-id',
-	type=int,
-	help='Specifies the offset id. This will update Telegram data with new posts.'
-)
-
-
-
 # parse arguments
 args = vars(parser.parse_args())
 config_attrs = get_config_attrs()
 
 args = {**args, **config_attrs}
-
-if all(i is not None for i in args.values()):
-	parser.error('Select either --telegram-channel or --batch-file options only.')
 
 
 # log results
@@ -104,6 +87,8 @@ api_hash = args['api_hash']
 phone = args['phone']
 
 # event loop
+# Change by congosto
+nest_asyncio.apply()
 loop = asyncio.get_event_loop()
 
 # data collection
@@ -121,15 +106,7 @@ client = loop.run_until_complete(
 )
 
 # request type
-req_type, req_input = cmd_request_type(args)
-if req_type == 'batch':
-	req_input = [
-		i.rstrip() for i in open(
-			req_input, encoding='utf-8', mode='r'
-		)
-	]
-else:
-	req_input = [req_input]
+channel = args['telegram_channel']
 
 # reading | Creating an output folder
 if args['output']:
@@ -154,188 +131,201 @@ Methods
 
 '''
 
+#change by congosto
+'''
+Channels are downloaded one by one
+'''
+	
+# add by congosto
+'''
+get context download
+'''
+(start_msg,num_msgs) = get_last_download_context(f'{output_folder}/{channel}/log_downloads.csv')
 
-# iterate channels
-for channel in req_input:
+'''
 
-	'''
+Process arguments
+-> channels' data
 
-	Process arguments
-	-> channels' data
+-> Get Entity <Channel's attrs>
+-> Get Full Channel request.
+-> Get Posts <Request channels' posts>
 
-	-> Get Entity <Channel's attrs>
-	-> Get Full Channel request.
-	-> Get Posts <Request channels' posts>
+'''
 
-	'''
+# new line
+print ('')
+print (f'> Collecting data from Telegram Channel -> {channel}')
+print ('> ...')
+print ('')
 
-	# new line
-	print ('')
-	print (f'> Collecting data from Telegram Channel -> {channel}')
-	print ('> ...')
-	print ('')
+# Channel's attributes
+entity_attrs = loop.run_until_complete(
+	get_entity_attrs(client, channel)
+)
 
-	# Channel's attributes
-	entity_attrs = loop.run_until_complete(
-		get_entity_attrs(client, channel)
+
+
+if entity_attrs:
+
+	# Get Channel ID | convert output to dict
+	channel_id = entity_attrs.id
+	entity_attrs_dict = entity_attrs.to_dict()
+
+	# Collect Source -> GetFullChannelRequest
+	channel_request = loop.run_until_complete(
+		full_channel_req(client, channel_id)
 	)
 
-	if entity_attrs:
+	# save full channel request
+	full_channel_data = channel_request.to_dict()
 
-		# Get Channel ID | convert output to dict
-		channel_id = entity_attrs.id
-		entity_attrs_dict = entity_attrs.to_dict()
+	# JsonEncoder
+	full_channel_data = JSONEncoder().encode(full_channel_data)
+	full_channel_data = json.loads(full_channel_data)
 
-		# Collect Source -> GetFullChannelRequest
-		channel_request = loop.run_until_complete(
-			full_channel_req(client, channel_id)
-		)
+	# save data
+	print ('> Writing channel data...')
+	create_dirs(output_folder, subfolders=channel)
+	file_path = f'{output_folder}/{channel}/{channel}.json'
+	channel_obj = json.dumps(
+		full_channel_data,
+		ensure_ascii=False,
+		separators=(',',':')
+	)
+	writer = open(file_path, mode='w', encoding='utf-8')
+	writer.write(channel_obj)
+	writer.close()
+	print ('> done.')
+	print ('')
 
-		# save full channel request
-		full_channel_data = channel_request.to_dict()
+	# collect chats
+	chats_path = f'{output_folder}/chats.txt'
+	chats_file = open(chats_path, mode='a', encoding='utf-8')
 
+	# channel chats
+	counter = write_collected_chats(
+	full_channel_data['chats'],
+		chats_file,
+		channel,
+		counter,
+		'channel_request',
+		client,
+		output_folder
+	)
+	last_msg = start_msg
+	num_msgs = 0
+	min_id = start_msg
+	if not args['limit_download_to_channel_metadata']:
+		# Collect posts
+		if start_msg == 0: # changed by congosto
+			print('Downloading all messages')
+			posts = loop.run_until_complete(
+				get_posts(client, channel_id)
+			)
+
+		else:
+			print(f'Downloading from msg {start_msg}')
+			posts = loop.run_until_complete(
+				get_posts(client, channel_id, min_id=min_id)
+			)
+
+		data = posts.to_dict()
+		# Change by Congosto
+		'''
+		get most recent msg 
+		'''
+		if len(posts.messages) > 0:
+			offset_id = min([i['id'] for i in data['messages']])
+			last_msg = data['messages'][0]['id']
+			num_msgs = len(posts.messages)
+		# Get offset ID | Get messages
+		while len(posts.messages) > 0:
+
+			if start_msg > 0: # changed by congosto
+				posts = loop.run_until_complete(
+					get_posts(
+						client,
+						channel_id,
+						min_id=min_id,
+						offset_id=offset_id
+					)
+				)	
+			else:
+				posts = loop.run_until_complete(
+					get_posts(
+						client,
+						channel_id,
+						offset_id=offset_id
+					)
+				)
+			# Update data dict
+			if posts.messages:
+				tmp = posts.to_dict()
+				data['messages'].extend(tmp['messages'])
+
+				# Adding unique chats objects
+				all_chats = [i['id'] for i in data['chats']]
+				chats = [
+					i for i in tmp['chats']
+					if i['id'] not in all_chats
+				]
+				# channel chats in posts
+				counter = write_collected_chats(
+					tmp['chats'],
+					chats_file,
+					channel,
+					counter,
+					'from_messages',
+					client,
+					output_folder
+				)
+				# Adding unique users objects
+				all_users = [i['id'] for i in data['users']]
+				users = [
+					i for i in tmp['users']
+					if i['id'] not in all_users
+				]
+				# extend UNIQUE chats & users
+				data['chats'].extend(chats)
+				data['users'].extend(users)
+				# Get offset ID
+				offset_id = min([i['id'] for i in tmp['messages']])
+				# Change by Congosto
+				'''
+				get most recent msg and show post downloaded
+				'''
+				num_msgs = num_msgs + len(posts.messages)
+				if num_msgs% 5000 == 0:
+					print(f'{num_msgs} downloaded\r')
+					#print(f'\x1b[2K{num_msgs} downloaded', end='\r')
 		# JsonEncoder
-		full_channel_data = JSONEncoder().encode(full_channel_data)
-		full_channel_data = json.loads(full_channel_data)
+		print('\n') # no quitar
+		data = JSONEncoder().encode(data)
+		data = json.loads(data)
 
 		# save data
-		print ('> Writing channel data...')
-		create_dirs(output_folder, subfolders=channel)
-		file_path = f'{output_folder}/{channel}/{channel}.json'
-		channel_obj = json.dumps(
-			full_channel_data,
+		print ('> Writing posts data...')
+		file_path = f'{output_folder}/{channel}/{channel}_messages.json'
+		obj = json.dumps(
+			data,
 			ensure_ascii=False,
 			separators=(',',':')
 		)
-		writer = open(file_path, mode='w', encoding='utf-8')
-		writer.write(channel_obj)
-		writer.close()
-		print ('> done.')
-		print ('')
-
-		# collect chats
-		chats_path = f'{output_folder}/chats.txt'
-		chats_file = open(chats_path, mode='a', encoding='utf-8')
-
-		# channel chats
-		counter = write_collected_chats(
-			full_channel_data['chats'],
-			chats_file,
-			channel,
-			counter,
-			'channel_request',
-			client,
-			output_folder
-		)
-
-		if not args['limit_download_to_channel_metadata']:
-
-			# Collect posts
-			if not args['min_id']:
-				posts = loop.run_until_complete(
-					get_posts(client, channel_id)
-				)
-
-			else:
-				min_id = args['min_id']
-				posts = loop.run_until_complete(
-					get_posts(client, channel_id, min_id=min_id)
-				)
-
-			data = posts.to_dict()
-
-			# Get offset ID | Get messages
-			offset_id = min([i['id'] for i in data['messages']])
-
-			while len(posts.messages) > 0:
-				
-				if args['min_id']:
-					posts = loop.run_until_complete(
-						get_posts(
-							client,
-							channel_id,
-							min_id=min_id,
-							offset_id=offset_id
-						)
-					)	
-				else:
-					posts = loop.run_until_complete(
-						get_posts(
-							client,
-							channel_id,
-							offset_id=offset_id
-						)
-					)
-
-				# Update data dict
-				if posts.messages:
-					tmp = posts.to_dict()
-					data['messages'].extend(tmp['messages'])
-
-					# Adding unique chats objects
-					all_chats = [i['id'] for i in data['chats']]
-					chats = [
-						i for i in tmp['chats']
-						if i['id'] not in all_chats
-					]
-
-					# channel chats in posts
-					counter = write_collected_chats(
-						tmp['chats'],
-						chats_file,
-						channel,
-						counter,
-						'from_messages',
-						client,
-						output_folder
-					)
-
-					# Adding unique users objects
-					all_users = [i['id'] for i in data['users']]
-					users = [
-						i for i in tmp['users']
-						if i['id'] not in all_users
-					]
-
-					# extend UNIQUE chats & users
-					data['chats'].extend(chats)
-					data['users'].extend(users)
-
-					# Get offset ID
-					offset_id = min([i['id'] for i in tmp['messages']])
-
-			# JsonEncoder
-			data = JSONEncoder().encode(data)
-			data = json.loads(data)
-
-			# save data
-			print ('> Writing posts data...')
-			file_path = f'{output_folder}/{channel}/{channel}_messages.json'
-			obj = json.dumps(
-				data,
-				ensure_ascii=False,
-				separators=(',',':')
-			)
 			
-			# writer
-			writer = open(file_path, mode='w', encoding='utf-8')
-			writer.write(obj)
-			writer.close()
-			print ('> done.')
-			print ('')
+		# writer
+		writer = open(file_path, mode='w', encoding='utf-8')
+		writer.write(obj)
+		writer.close()
 
-		# sleep program for a few seconds
-		if len(req_input) > 1:
-			time.sleep(2)
-	else:
-		'''
+	'''
 
-		Channels not found
-		'''
-		exceptions_path = f'{output_folder}/_exceptions-channels-not-found.txt'
-		w = open(exceptions_path, encoding='utf-8', mode='a')
-		w.write(f'{channel}\n')
-		w.close()
+	Channels not found
+	'''
+	exceptions_path = f'{output_folder}/_exceptions-channels-not-found.txt'
+	w = open(exceptions_path, encoding='utf-8', mode='a')
+	w.write(f'{channel}\n')
+	w.close()
 
 '''
 
@@ -379,22 +369,81 @@ counter_df.to_csv(
 
 # merge dataframe
 df = pd.read_csv(
-	f'{output_folder}/collected_chats.csv',
-	encoding='utf-8'
+f'{output_folder}/collected_chats.csv',
+encoding='utf-8'
 )
 
-del counter_df['username']
-df = df.merge(counter_df, how='left', on='id')
+#del counter_df['username'] # Change by Congosto
+#remove possible duplicates
+df.drop_duplicates(subset=['id'], keep='first', inplace=True) # Change by Congosto
 df.to_csv(
 	f'{output_folder}/collected_chats.csv',
+	mode='w', # Change by Congosto  avoid duplicates
 	index=False,
 	encoding='utf-8'
 )
 
-
-# log results
-text = f'''
-End program at {time.ctime()}
-
+# Change by Congosto
 '''
+Save names of related channels
+'''
+users_names = df["username"]
+users_names.to_csv(
+	f'{output_folder}/related_channels.csv',
+	mode='w', 
+	header=False,
+	index=False,
+	encoding='utf-8'
+)
+df = df.merge(counter_df, how='left', on='id')
+'''
+The metadata and counters are saved in collected_chats_full.csv,
+to avoid problems when making 
+ successive merges when extracting the channel data again
+'''
+df.to_csv(
+	f'{output_folder}/collected_chats_full.csv',
+	mode='w', # Change by Congosto para que no duplique entradas
+	index=False,
+	encoding='utf-8'
+)
+'''
+The metadata and counters are saved in collected_chats_full.csv,
+to avoid problems when making 
+ successive merges when extracting the channel data again
+'''
+df.to_csv(
+	f'{output_folder}/collected_chats_full.csv',
+	mode='w', 
+	index=False,
+	encoding='utf-8'
+)
+# changed by congosto
+'''
+Save downloaded context
+'''
+put_last_download_context(f'{output_folder}/{channel}/log_downloads.csv',time.ctime(),last_msg,num_msgs)
+# log results
+text = f'End program at {time.ctime()}'
+
+# changed by congosto
+'''
+Save collected_channel
+'''
+root_data = output_folder.split('/')
+root_data = root_data[1]
+store_channels_download(f'./{root_data}/collected_channel_all.csv',channel,output_folder)
+# log results
+# changed by congosto
+'''
+Save related_channel
+'''
+root_data = output_folder.split('/')
+root_data = root_data[1]
+store_channels_related(f'./{root_data}/related_channel_all.csv',users_names,output_folder)
+# log results
+text = f'End program at {time.ctime()}'
+
+
+
 print (text)
